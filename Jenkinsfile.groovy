eb76311f6f3f07b8feb32022683e0a87320ff14e
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-16 16:17:24 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ node{
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/simple-java-maven-app.git'
     withMaven(
         // Maven installation declared in the Jenkins "Global Tool Configuration"
-        maven: 'M3',
+        maven: 'M3')
         
       bat "mvn clean install"
  
