76096ac765c72488908ad5d2f71da258e78df13b
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-16 16:10:00 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,18 @@
-node('master') {
+node{
+  stage ('Build') {
+ 
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/simple-java-maven-app.git'
-    bat label: '', script: 'mvn build'
+    withMaven(
+        // Maven installation declared in the Jenkins "Global Tool Configuration"
+        maven: 'M3',
+        // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
+        // Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
+        //mavenSettingsConfig: 'my-maven-settings',
+        mavenLocalRepo: '.repository') {
+ 
+      // Run the maven build
+      sh "mvn clean install"
+ 
+    } // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe & FindBugs reports...
+  }
 }
