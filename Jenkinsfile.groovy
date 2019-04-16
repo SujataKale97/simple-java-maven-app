9f2098d913bf3b5974140736e5f2295514818ec0
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-16 16:15:47 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,15 +5,9 @@ node{
     withMaven(
         // Maven installation declared in the Jenkins "Global Tool Configuration"
         maven: 'M3',
-        // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
-        // Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
-        //mavenSettingsConfig: 'my-maven-settings',
-        mavenLocalRepo: '.repository') {
+        
+      bat "mvn clean install"
  
-      // Run the maven build
-     // sh "mvn clean install"
-      bat 'mvn build'
- 
-    } // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe & FindBugs reports...
+    }
   }
 }
