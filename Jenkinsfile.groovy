52262452d28ec49a186c965c32d0e9a54707063e
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-04-16 16:05:02 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,4 @@
 node('master') {
     git credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c', url: 'https://github.com/SujataKale97/simple-java-maven-app.git'
-    bat label: '', script: 'mvn verify'
+    bat label: '', script: 'mvn build'
 }
